5c85dd57265ed20eb5f2b52a8052f2f311a5cc49
Junghoon Cho <junghooncho@Junghoons-MacBook-Pro.local>
2025-05-06 15:41:41 -0400

    Set up portfolio structure

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
+/**@type {import('next').NextConfig}*/
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   trailingSlash: true,
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ?
+      '/jun001107.github.io': '',
 };
 
 module.exports = nextConfig;
-export default nextConfig;
