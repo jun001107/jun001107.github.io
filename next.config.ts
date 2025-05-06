86308862ff07e9e36087f78e95b6812db7266eda
Junghoon Cho <junghooncho@Junghoons-MacBook-Pro.local>
2025-05-06 15:59:46 -0400

    Set up portfolio structure

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === 'production' ?
-      '/jun001107.github.io': '',
 };
 
 module.exports = nextConfig;
