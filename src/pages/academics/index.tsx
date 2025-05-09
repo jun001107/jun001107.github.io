8f0fb84642c96fb2ae4d776c0c684af5688bd8ba
Junghoon Cho <junghooncho@Junghoons-MacBook-Pro.local>
2025-05-09 15:14:05 -0400

    Fixed error

---
+++ src/pages/academics/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+
+const Academics: NextPage = () => {
+    return (
+        <section>
+            <h2>Contact</h2>
+            <p>
+                ✉️ Email: <a href="mailto:junghoon.cho@mail.mcgill.ca">junghoon.cho@mail.mcgill.ca</a>
+            </p>
+            <p>
+                🔗 LinkedIn:{' '}
+                <a href="https://www.linkedin.com/in/junghooncho117" target="_blank" rel="noopener noreferrer">
+                    Junghoon Cho
+                </a>
+            </p>
+        </section>
+    )
+}
+
+export default Academics
